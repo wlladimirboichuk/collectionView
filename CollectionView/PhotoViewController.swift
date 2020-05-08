9e58251d72bade34858167d723b53253c2b1fe37
wlladimirboichuk <wlladimirboichuk@gmail.com>
2020-05-09 01:53:18 +0300

    Detail Screen Setup

---
+++ CollectionView/PhotoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoViewController.swift
+//  CollectionView
+//
+//  Created by Volodymyr Boichuk on 09.05.2020.
+//  Copyright © 2020 Volodymyr Boichuk. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
