d9c318bae2a60b087d324da9e0b7b6512b842956
wlladimirboichuk <wlladimirboichuk@gmail.com>
2020-05-08 21:30:42 +0300

    first commit

---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Volodymyr Boichuk on 08.05.2020.
+//  Copyright © 2020 Volodymyr Boichuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
