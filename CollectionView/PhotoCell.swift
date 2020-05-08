d1262626a530ebf37aab02f00199d0da03754655
wlladimirboichuk <wlladimirboichuk@gmail.com>
2020-05-08 23:53:29 +0300

    Collection Configuration

---
+++ CollectionView/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  CollectionView
+//
+//  Created by Volodymyr Boichuk on 08.05.2020.
+//  Copyright © 2020 Volodymyr Boichuk. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
